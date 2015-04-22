26b6a05e19186a71c86e66debaa311cd1a4f318c
Ron Tsui <ronsuij@gmail.com>
2015-04-21 20:36:33 -0700

    developed index.html and main.js pages

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MainCtrl', ['$scope', function ($scope){...}]);
-myApp.controller('NavCtrl', ['$scope', function ($scope){...}]);
-myApp.controller('UserCtrl', ['$scope', function ($scope){...}]);
+myApp.controller('MainCtrl', ['$scope', function ($scope){
+  %scope.text = 'Hello, Angular fanatic.';
+}]);
+
+
+// myApp.controller('NavCtrl', ['$scope', function ($scope){...}]);
+// myApp.controller('UserCtrl', ['$scope', function ($scope){...}]);
 
 
