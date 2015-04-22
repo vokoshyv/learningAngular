0d11215d378036ec854a15bd61f1d79cf79036bb
Ron Tsui <ronsuij@gmail.com>
2015-04-21 20:08:36 -0700

    set up initial files for learning angular

---
+++ main.js
@@ -0,0 +1,7 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MainCtrl', ['$scope', function ($scope){...}]);
+myApp.controller('NavCtrl', ['$scope', function ($scope){...}]);
+myApp.controller('UserCtrl', ['$scope', function ($scope){...}]);
+
+
